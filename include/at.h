fa610fde670fff9b6ab5dd038e48682b61ac8287
Jan Janak <jan@janakj.org>
2021-11-24 10:15:02 +0100

    Backport most recent AT commands from lora-tester
    This patch backports the most recent LoRa AT command implementation from
    the lora tester to twr-lora-climate-pir-co2.

--- include/at.h
+++ include/at.h
@@ -14,7 +14,18 @@
                          {"$BAND", NULL, at_band_set, at_band_read, NULL, "0:AS923, 1:AU915, 5:EU868, 6:KR920, 7:IN865, 8:US915"},\
                          {"$MODE", NULL, at_mode_set, at_mode_read, NULL, "0:ABP, 1:OTAA"},\
                          {"$NWK", NULL, at_nwk_set, at_nwk_read, NULL, "Network type 0:private, 1:public"},\
-                         {"$JOIN", at_join, NULL, NULL, NULL, "Send OTAA Join packet"}
+                         {"$ADR", NULL, at_adr_set, at_adr_read, NULL, "Automatic data rate 0:disabled, 1:enabled"},\
+                         {"$DR", NULL, at_dr_set, at_dr_read, NULL, "Data rate 0-15"},\
+                         {"$REPU", NULL, at_repu_set, at_repu_read, NULL, "Repeat of unconfirmed transmissions 1-15"},\
+                         {"$REPC", NULL, at_repc_set, at_repc_read, NULL, "Repeat of confirmed transmissions 1-8"},\
+                         {"$JOIN", at_join, NULL, NULL, NULL, "Send OTAA Join packet"},\
+                         {"$FRMCNT", at_frmcnt, NULL, NULL, NULL, "Get frame counters"},\
+                         {"$LNCHECK", at_link_check, NULL, NULL, NULL, "MAC Link Check"},\
+                         {"$RFQ", at_rfq, NULL, NULL, NULL, "Get RSSI/SNR of last RX packet"},\
+                         {"$REBOOT", at_reboot, NULL, NULL, NULL, "Firmware reboot"},\
+                         {"$FRESET", at_freset, NULL, NULL, NULL, "LoRa Module factory reset"},\
+                         {"$FWVER", at_ver_read, NULL, NULL, NULL, "Show LoRa Module firmware version"}
+
 
 #define AT_LED_COMMANDS {"$BLINK", at_blink, NULL, NULL, NULL, "LED blink 3 times"},\
                         {"$LED", NULL, at_led_set, NULL, at_led_help, "LED on/off"}
@@ -47,6 +58,26 @@ bool at_mode_set(twr_atci_param_t *param);
 
 bool at_nwk_read(void);
 bool at_nwk_set(twr_atci_param_t *param);
+
+bool at_adr_read(void);
+bool at_adr_set(twr_atci_param_t *param);
+
+bool at_dr_read(void);
+bool at_dr_set(twr_atci_param_t *param);
+
+bool at_repu_read(void);
+bool at_repu_set(twr_atci_param_t *param);
+
+bool at_repc_read(void);
+bool at_repc_set(twr_atci_param_t *param);
+
+bool at_ver_read(void);
+
+bool at_reboot(void);
+bool at_freset(void);
+bool at_frmcnt(void);
+bool at_link_check(void);
+bool at_rfq(void);
 
 bool at_join(void);
 bool at_blink(void);
